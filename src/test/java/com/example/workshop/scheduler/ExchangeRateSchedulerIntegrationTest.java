edeea40071c5677b4d3b211553869804e80eff28
r.aleksandrou <raman.aleksandrou@masternaut.com>
2025-12-30 12:06:41 +0100

    fix: Remove create-drop from ExchangeRateSchedulerIntegrationTest
    - ExchangeRateSchedulerIntegrationTest was using spring.jpa.hibernate.ddl-auto=create-drop
    - This bypassed Liquibase migrations and caused new indexes to not be created
    - Removed the property to let Liquibase handle schema management
    - Tests now use proper Liquibase migrations like other integration tests

--- src/test/java/com/example/workshop/scheduler/ExchangeRateSchedulerIntegrationTest.java
+++ src/test/java/com/example/workshop/scheduler/ExchangeRateSchedulerIntegrationTest.java
@@ -20,8 +20,7 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 @TestPropertySource(properties = {
         "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
+        "spring.datasource.driver-class-name=org.h2.Driver"
 })
 class ExchangeRateSchedulerIntegrationTest {
 
